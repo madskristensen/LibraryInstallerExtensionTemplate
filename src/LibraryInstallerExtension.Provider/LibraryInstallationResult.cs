1ba7129916ffdd688609dec8e700c997ea8f0898
Mads Kristensen <post@madskristensen.net>
2017-04-13 09:18:14 -0700

    Add project files.

---
+++ src/LibraryInstallerExtension.Provider/LibraryInstallationResult.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Web.LibraryInstaller.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryInstallerExtension.Provider
+{
+    internal class LibraryInstallationResult : ILibraryInstallationResult
+    {
+        public LibraryInstallationResult(ILibraryInstallationState installationState)
+        {
+            Errors = new List<IError>();
+            InstallationState = installationState;
+        }
+
+        public LibraryInstallationResult(ILibraryInstallationState installationState, params IError[] error)
+        {
+            var list = new List<IError>();
+            list.AddRange(error);
+            Errors = list;
+            InstallationState = installationState;
+        }
+
+        public bool Cancelled
+        {
+            get;
+            set;
+        }
+
+        public bool Success
+        {
+            get { return !Cancelled && !Errors.Any(); }
+        }
+
+        public IList<IError> Errors
+        {
+            get;
+            set;
+        }
+
+        public ILibraryInstallationState InstallationState
+        {
+            get;
+            set;
+        }
+
+        public static LibraryInstallationResult FromSuccess(ILibraryInstallationState installationState)
+        {
+            return new LibraryInstallationResult(installationState);
+        }
+
+        public static LibraryInstallationResult FromCancelled(ILibraryInstallationState installationState)
+        {
+            return new LibraryInstallationResult(installationState)
+            {
+                Cancelled = true
+            };
+        }
+    }
+}
